a178c2c21c189561ee6dca63f33b6d234d3ea373
Ginno Taimal <ginno.taimal@epn.edu.ec>
2023-06-20 22:19:48 -0500

    feat: Add Android Exercises

---
+++ 01 - Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.movilessoftware2023a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        botonCicloVida.setOnClickListener {
+            irActividad(AACicloVida::class.java)
+        }
+    }
+
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+        // this.startActivity()
+    }
+}
